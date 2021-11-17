d952994424812af1d612b1795d0741d75826ae16
Yudhistira Yoga Vacara <yudhistira1594yogavacara@gmail.com>
2021-11-17 20:11:19 +0700

    adding new 5 pages

---
+++ profile.php
@@ -0,0 +1,22 @@
+<?php
+    // header
+    echo "<h1>Sacode</h1>";
+    echo "<p>#LearnToCode</p>";
+    echo "<hr>";
+
+    // navigasi
+    echo "<a href='index.php'>Home</a>    ";
+    echo "<a href='profile.php'>Profile</a>    ";
+    echo "<a href='gallery.php'>Gallery</a>    ";
+    echo "<a href='shop.php'>Shop</a>    ";
+    echo "<a href='contact.php'>Contact</a>    ";
+
+    // main content
+    echo "<h2>Profile Page</h2>";
+    echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias tempore itaque optio quia, ipsa architecto accusantium. Incidunt nesciunt similique suscipit dignissimos in. Recusandae molestias doloremque aspernatur fugiat id sit nisi.</p>";
+
+    // footer
+    echo "<small>Copyright &copy; 2021 </small>";
+
+?>
+
